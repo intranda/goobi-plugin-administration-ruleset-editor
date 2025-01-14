f95bb48929a57f725a99356d9590c7018034dfa1
Paul Hankiewicz Lopez <paul@intranda.com>
2025-01-14 14:16:24 +0100

    Removed test.java file and renamed java class

--- module-base/src/main/java/de/intranda/goobi/plugins/RulesetEditorAdministrationPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/RulesetEditorAdministrationPlugin.java
@@ -352,7 +352,7 @@ public class RulesetEditorAdministrationPlugin implements IAdministrationPlugin 
             XPathFactory xPathFactory = XPathFactory.newInstance();
             XPath xpath = xPathFactory.newXPath();
 
-            RulesetToJava.parseAndCheckDocument(document);
+            validateDocstructTypePartInRuleset.parseAndCheckDocument(document);
 
             // ERROR: undefined but used
             String errorDescription = Helper.getTranslation("ruleset_validation_undefined_metadata_but_used");
